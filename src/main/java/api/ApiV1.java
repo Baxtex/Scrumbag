457aed8045b81b2310cf8cae8c19f33071520a05
Anton Gustafsson <anton.gustafsson@protonmail.com>
2017-02-28 13:12:12 +0100

    Added more example code and corrected a few mistakes

--- src/main/java/api/ApiV1.java
+++ src/main/java/api/ApiV1.java
@@ -2,10 +2,24 @@ package api;
 
 import static spark.Spark.get;
 
-public class ApiV1 {
+import org.codehaus.jettison.json.JSONObject;
 
+public class ApiV1 {
+	// Good tutorial: https://dzone.com/articles/building-simple-restful-api
 	public static void main(String[] args) {
-		// The ULR for this is: http://localhost:4567/hello
-		get("/hello", (req, res) -> "Hello World");
+		// The URL for this is: http://localhost:4567/hello
+		get("/hello", (req, res) -> "Hello World!");
+		get("/helloAgain", (req, res) -> "Hello Again!");
+
+		// Create Json object and return it.
+		JSONObject jsonObject = new JSONObject();
+		try {
+			jsonObject.put("message", "Hello Json!");
+			jsonObject.put("antoher message", "Hello again Json!");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		get("/helloJson", (req, res) -> jsonObject);
 	}
 }
