bdb6c3f7766b899140514a921d622afdff1523f4
Strandberg95 <strandberg.coffe@gmail.com>
2017-03-07 19:46:00 +0100

    Temporary code to securityHandler has been added

---
+++ src/main/java/controller/SecurityHandler.java
@@ -0,0 +1,63 @@
+package controller;
+
+import java.util.Random;
+
+import database.SDBHandler;
+
+public class SecurityHandler {
+	
+	private SDBHandler dbHandler;
+	
+	/**
+	 * Other
+	 */
+	private Random rdm;
+	
+	private final String USERNAME_User = "Anton2005";
+	private final String PASSWORD_User = "coolkille1337";
+	private int key_User;
+	
+	private final String USERNAME_Admin = "Coffe1337";
+	private final String PASSWORD_Admin = "kisekatt123";
+	private int key_Admin;
+	
+	public SecurityHandler(){
+		dbHandler = new SDBHandler();
+		rdm = new Random();
+		dbHandler.connect();
+	}
+	
+	public int login(String username, String password){
+		if(username.equals(USERNAME_User) && password.equals(PASSWORD_User)){
+			key_User = rdm.nextInt() + 1; 
+			return key_User;			
+		}else if(username.equals(USERNAME_Admin) && password.equals(PASSWORD_Admin)){
+			key_Admin = rdm.nextInt();
+			return key_Admin;
+		}else{
+			
+			return 0;
+		}
+		
+	}
+	
+	public String logout(String key){
+		return key + " : Has been logged out!";
+	}
+	
+	public int checkAuthority(int key){
+		if(key == key_Admin){
+			return 2;
+		}else if(key == key_User){
+			return 1;
+		}else{
+			return 0;
+		}
+	}
+	
+	
+	public void validateSession(){
+		dbHandler.validateSession();
+	}
+	
+}
--- src/main/java/database/SDBHandler.java
+++ src/main/java/database/SDBHandler.java
@@ -1,5 +1,7 @@
 package database;
 
+import java.util.Random;
+
 /**
  * Database handler for the security database.
  * 
@@ -7,5 +9,23 @@ package database;
  *
  */
 public class SDBHandler {
+	
+	private Random rdm;
+	
+	public SDBHandler(){
+		rdm = new Random();
+	}
+
+	public void connect(){
+		System.out.println("Connected to Security Database");
+	}
+	
+	public void disconnect(){
+		System.out.println("Disconnected from Security Database");
+	}
+	
+	public void validateSession(){
+		System.out.println("Sessions updated! " + rdm.nextInt(6) + " sessions were removed");
+	}
 
 }
