7a32a85ac8d9b9fc4a2a89db0a7f55fc1d15492a
Strandberg95 <strandberg.coffe@gmail.com>
2017-03-17 16:36:50 +0100

    DataHandler updated

--- src/main/java/dataLayer/DataHandler.java
+++ src/main/java/dataLayer/DataHandler.java
@@ -30,16 +30,6 @@ public class DataHandler {
 	private LinkedList<Sprint> sprints;
 	private LinkedList<User> users;
 	
-	
-//	private Hashtable<String,String> user_project; //<UserID,ProjectId>
-//	private Hashtable<String,Project>
-//	private 
-	
-	
-	/** Kalles
-	 */
-	
-	
 	public DataHandler(){
 		idCounter = 0;
 		projects = new LinkedList<Project>();
@@ -48,52 +38,54 @@ public class DataHandler {
 		users = new LinkedList<User>();
 	}
 	
-	public boolean checkProjectId(String id) {
-		Project tmp;
-		for(int i = 0; i < projects.size(); i++){
-			tmp = projects.get(i);
-			if(tmp.getProjectId().equals(id)){
+	/**
+	 * Activity
+	 */
+	
+	public boolean checkActivityId(String aID) {
+		for(int i = 0; i < activities.size(); i++){
+			if(activities.get(i).getActivityId().equals(aID)){
 				return true;
 			}
 		}
 		return false;
 	}
+	
+	public String createActivity(String projectID,
+			String sprintID, String title, String description, String timeExpected,
+			String timeSpent, String respUser, Status status, Priority priority){
+		Activity activity = new Activity((idCounter++)+"", projectID, sprintID, title
+				, description, timeExpected, timeSpent, respUser, status, priority);
+		return activity.getActivityId();
 
-	public boolean checkActivityId(String id) {
-		if (id.equals("validActivityId")) {
-			return true;
-		} else {
-			return false;
+	}
+	
+	public void editActivity(String activityID, String projectID, String title, String description, 
+			String timeExpected,String timeSpent, String respUser, Status status, Priority priority){
+		Activity a = getActivity(activityID);
+		a.setProjectId(projectID);
+		a.setTitle(title);
+		a.setDescription(description);
+		a.setTimeExpected(timeExpected);
+		a.setTimeSpent(timeSpent);
+		a.setRespUser(respUser);
+		a.setStatus(status);
+		a.setPriority(priority);
+
+	}
+
+	public void deleteActivity(String aID) {
+		Activity tmp = null;
+		for(int i = 0; i < activities.size(); i++){
+			tmp = activities.get(i);
+			if(tmp.getActivityId().equals(aID)){
+				activities.remove(i);
+				break;
+			}
 		}
 	}
 
-	public boolean checkProjectName(String name) {
-		if (name.equals("validActivityName")) {
-			return true;
-		} else {
-			return false;
-		}
-	}
-
-	public boolean validUsers(JSONObject jsonObjUsers) {
-		String invalid = "";
-		try {
-			invalid = jsonObjUsers.getString("name");
-		} catch (JSONException e) {
-			e.printStackTrace();
-		}
-		if (invalid.contains("invalid")) {
-			return false;
-		} else {
-			return true;
-		}
-	}
-
-	public void deleteActivity() {
-		// TODO Auto-generated method stub
-	}
-
-	public Activity[] getActivities(String pID) { //Klar
+	public Activity[] getActivities(String pID) {
 		int arrSize = 0;
 		int arrIndex = 0;
 		Activity[] rArr;
@@ -118,7 +110,7 @@ public class DataHandler {
 		return rArr;
 	}
 
-	public Activity getActivity(String aID) { //Klar
+	private Activity getActivity(String aID) {
 		Activity rActivity = null;
 		for(int i = 0; i < activities.size(); i++) {
 			if(activities.get(i).getActivityId().equals(aID)){
@@ -128,39 +120,103 @@ public class DataHandler {
 		return rActivity;
 	}
 
-	public String createProject(String pName) { //Klar
+	/**
+	 * Project
+	 */
+	
+	public boolean checkProjectId(String ID) {
+		Project tmp;
+		for(int i = 0; i < projects.size(); i++){
+			tmp = projects.get(i);
+			if(tmp.getProjectId().equals(ID)){
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	public boolean checkProjectName(String pName) {
+		for(int i = 0; i < projects.size(); i++){
+			if(projects.get(i).getName().equals(pName)){
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	public String createProject(String pName) { 
 		Project tmp = new Project((idCounter++)+"",pName);
 		projects.add(tmp);
 		return tmp.getProjectId();
 	}
+	
+	private Project getProject(String pID){
+		Project tmp = null;
+		for(int i = 0; i < projects.size(); i++){
+			tmp = projects.get(i);
+			if(tmp.getProjectId().equals(pID))
+				break;
+		}
+		return tmp;
+	}
 
-	public String createSprint(String pID,String title, int index) { //Klar
+	public String createSprint(String pID,String title, int index) {
 		Sprint tmp = new Sprint((idCounter++) + "",pID,title,index);
 		sprints.add(tmp);
 		return tmp.getSprintId();
 	}
 	
-	public void addUser(String name) { //Klar
-		users.add(new User((idCounter++)+"",name));
+	public String createUser(String name) { 
+		User tmp = new User((idCounter++)+"",name);
+		users.add(tmp);
+		return tmp.getUserId();
 	}
 	
-	public void addUserToProject(String pID) {
-		
+	public void addUserToProject(String pID, String uID) {
+		Project p = getProject(pID);
+		User u = getUser(uID);
+		p.addUser(u);
+	}
+	
+	/**
+	 * User Methods
+	 */
+	public boolean validateUser(String uID){
+		for(int i = 0; i < users.size(); i++){
+			if(users.get(i).getUserId().equals(uID)){
+				return true;
+			}
+		}
+		return false;
 	}
 	
 	public User[] getUsers() {
-		
+		return (User[])users.toArray();
 	}
 	
-	public User[] getUsers(String pId){
-		
+	public User[] getUsers(String pID){
+		Project p = getProject(pID);
+		return (User[])p.getUsers().toArray();
+	}
+	
+	private User getUser(String uID){
+		User tmp = null;
+		for(int i = 0; i < users.size(); i++){
+			tmp = users.get(i);
+			if(tmp.getUserId().equals(uID)){
+				break;
+			}
+		}
+		return tmp;
 	}
 
-	public void removeUsers() {
-		// TODO Auto-generated method stub
+	public boolean removeUser(String uID) {
+		for(int i = 0; i < users.size(); i++){
+			if(users.get(i).getUserId().equals(uID)){
+				return true;
+			}
+		}
+		return false;
 	}
-
-	public void editActivity() {
-		// TODO Auto-generated method stub
-	}
+	
 }
