704d0c49356a6c7bb32037429ed0c9099f109ec6
Kajsa Ornstein <kajsaorn@gmail.com>
2017-03-17 19:33:55 +0100

    create activity end point

--- src/main/java/businessLayer/Post.java
+++ src/main/java/businessLayer/Post.java
@@ -1,12 +1,18 @@
 package businessLayer;
 
+import java.util.Arrays;
+import java.util.LinkedList;
+
 import org.codehaus.jettison.json.JSONObject;
 
+import dataLayer.Activity;
 import dataLayer.DataHandler;
+import dataLayer.DataHandler.Priority;
+import dataLayer.DataHandler.Status;
 import spark.Response;
 
 public class Post {
-
+	
 	private final DataHandler dataHandler;
 
 	public Post(DataHandler dataHandler) {
@@ -48,4 +54,34 @@ public class Post {
 		res.type("application/json");
 		return jObj;
 	}
+	
+//	public JSONObject createActivity(String projectID, String sprintID, String title, 
+//									String description, String timeExpected, String timeSpent, 
+//									String respUser, Status status, Priority priority, Response res) {		
+//		
+//		JSONObject jObj = new JSONObject();
+//		try {
+//						
+//			if (dataHandler.checkProjectId(projectID)) {
+//				if(checkStatus(status))
+//				String activityID = dataHandler.createActivity(projectID, sprintID, title, description, timeExpected, timeSpent, respUser, status, priority);
+//				jObj.put("Activity-ID", activityID);
+//				res.status(201);
+//			} else {
+//				jObj.put("message", "project does not exist");
+//				res.status(404);
+//			}
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		}
+//		res.type("application/json");
+//		return jObj;
+//	}
+//	
+//	private Status getStatus(String status) {
+//		
+//		
+//		
+//		return null;
+//	}
 }
